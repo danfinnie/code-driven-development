e3fbd093d13172967520a9cea79b109804e03db5
Dan Finnie <dan@danfinnie.com>
2014-02-13 22:02:21 -0500

    Just require everything.

--- code_driven_development.gemspec
+++ code_driven_development.gemspec
@@ -19,6 +19,7 @@ Gem::Specification.new do |spec|
   spec.require_paths = ["lib"]
 
   spec.add_dependency "ruby_parser"
+  spec.add_dependency "require_all"
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
--- lib/code_driven_development.rb
+++ lib/code_driven_development.rb
@@ -1,20 +1,4 @@
 require 'ruby_parser'
+require 'require_all'
 
-require "code_driven_development/version"
-require "code_driven_development/code_driven_development"
-require "code_driven_development/indented_output"
-
-require "code_driven_development/test_component/test"
-require "code_driven_development/test_component/one_line_test"
-require "code_driven_development/test_component/context"
-require "code_driven_development/test_component/blank_slate"
-require "code_driven_development/test_component/let"
-
-require "code_driven_development/rule/abstract_rule"
-require "code_driven_development/rule/set"
-require "code_driven_development/rule/method_call"
-require "code_driven_development/rule/default"
-require "code_driven_development/rule/boolean_or"
-require "code_driven_development/rule/class"
-require "code_driven_development/rule/validation"
-require "code_driven_development/rule/instance_method"
+require_rel "code_driven_development"
